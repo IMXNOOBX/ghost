51eb79c90d01e877b09cd7b315965e1948d0b722
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-04-10 10:24:56 +0200

    Moved folder creation directory up so it creates the directory first and then downloads everything

--- installer/installer.cmd
+++ installer/installer.cmd
@@ -6,6 +6,9 @@ set "repoUrl=https://api.github.com/repos/IMXNOOBX/ghost/releases/latest"
 set "installPath=%APPDATA%\Ghost"
 set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Ghost.lnk"
 
+rem Create installation directory if it doesn't exist
+if not exist "%installPath%" mkdir "%installPath%"
+
 rem Download latest release from GitHub
 powershell -Command "$response = Invoke-WebRequest -Uri '%repoUrl%' -UseBasicParsing; if ($response.StatusCode -eq 200) { $downloadUrl = ($response.Content | ConvertFrom-Json).assets.browser_download_url; Invoke-WebRequest -Uri $downloadUrl -OutFile '%installPath%\temp.zip'; if ($?) { exit 0 } else { exit 1 } } else { exit 1 }"
 
@@ -14,9 +17,6 @@ if %errorlevel% neq 0 (
     echo Error: Failed to download the latest release.
     exit /b 1
 )
-
-rem Create installation directory if it doesn't exist
-if not exist "%installPath%" mkdir "%installPath%"
 
 rem Extract files to destination
 powershell -Command "Expand-Archive -Path '%installPath%\temp.zip' -DestinationPath '%installPath%' -Force"
